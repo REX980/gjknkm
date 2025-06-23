5a8624794fb12824ffcd8e9e5fe856f097f42310
REX980 <logwire05@gmail.com>
2025-06-23 14:14:47 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Import Firebase modules
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, sendEmailVerification } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAcgbhGfk7CFuXrRb1yp8TVHS_CKHSb2TA",
+//   authDomain: "gamehubtournament-cff13.firebaseapp.com",
+//   projectId: "gamehubtournament-cff13",
+//   storageBucket: "gamehubtournament-cff13.appspot.com",
+//   messagingSenderId: "206218478452",
+//   appId: "1:206218478452:web:08d72bede942b6cf772221",
+//   measurementId: "G-FWE649GJ23"
+// };
+
+// Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+
+// Register Function
+window.register = function() {
+  alert("Register clicked!");
+}
+
+// Login Function
+window.login = function() {
+  alert("Login clicked!");
+}
